c1945943755b567acc2ffc354d674fb48a54c327
el_revelador <bsse1011@iit.du.ac.bd>
2020-11-25 04:14:02 +0600

    working with controllers

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NET_Core_Tutorial.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_Core_Tutorial.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
